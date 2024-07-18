a49e55294cbdf2ae82773eb5d11ca0c6a0caba5c
Krryxa <krryxq@163.com>
2024-07-18 16:38:39 +0800

    feat: 预加载脚本

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -8,6 +8,11 @@ defineProps<{
   <div class="greetings">
     <h1 class="green">{{ msg }}</h1>
     <h3>
+      Preload script：Node.js <span id="node-version"></span>, Chromium
+      <span id="chrome-version"></span>, and Electron
+      <span id="electron-version"></span>.
+    </h3>
+    <h3>
       You’ve successfully created a project with
       <a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a> +
       <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>. What's next?
@@ -25,6 +30,7 @@ h1 {
 
 h3 {
   font-size: 1.2rem;
+  margin-bottom: 12px;
 }
 
 .greetings h1,
