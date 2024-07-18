122e77f54ca75f05a35af345691b0262d9dbc1da
Krryxa <krryxq@163.com>
2024-07-18 15:48:49 +0800

    feat: app名称和icon

--- forge.config.js
+++ forge.config.js
@@ -3,13 +3,15 @@ import { FuseV1Options, FuseVersion } from '@electron/fuses';
 
 export default {
   packagerConfig: {
+    name: '灵魂莲华',
     asar: true,
+    icon: './electron/images/icon'
   },
   rebuildConfig: {},
   makers: [
     {
       name: '@electron-forge/maker-squirrel',
-      config: {},
+      config: {}
     },
     {
       name: '@electron-forge/maker-dmg',
@@ -20,21 +22,21 @@ export default {
     },
     {
       name: '@electron-forge/maker-zip',
-      platforms: ['darwin'],
+      platforms: ['darwin']
     },
     {
       name: '@electron-forge/maker-deb',
-      config: {},
+      config: {}
     },
     {
       name: '@electron-forge/maker-rpm',
-      config: {},
-    },
+      config: {}
+    }
   ],
   plugins: [
     {
       name: '@electron-forge/plugin-auto-unpack-natives',
-      config: {},
+      config: {}
     },
     // Fuses are used to enable/disable various Electron functionality
     // at package time, before code signing the application
@@ -45,7 +47,7 @@ export default {
       [FuseV1Options.EnableNodeOptionsEnvironmentVariable]: false,
       [FuseV1Options.EnableNodeCliInspectArguments]: false,
       [FuseV1Options.EnableEmbeddedAsarIntegrityValidation]: true,
-      [FuseV1Options.OnlyLoadAppFromAsar]: true,
-    }),
-  ],
-};
+      [FuseV1Options.OnlyLoadAppFromAsar]: true
+    })
+  ]
+}
